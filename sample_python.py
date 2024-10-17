fcf574a6b37b91bc3eedc202a5657fe87b788052
TejaSwaroopM-MAQ <mullamuris@maqsoftware.com>
2024-10-17 16:05:06 +0530

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -23,3 +23,4 @@ if __name__ == "__main__":
     print(calculator('divide', 5, 3))     # Output: 1.666...
     print(calculator('divide', 5, 0))     # Output: Error: Division by zero
     print(calculator('modulus', 5, 3))    # Output: Error: Unsupported operation
+    print(calculator('power', 3, 4))      # Output: 81
